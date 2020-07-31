5980f16cccdaba373e75733e84ac53b736b3c86a
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-08-01 01:12:06 +0530

    redux form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
-const store = createStore(reducers);
+const composeEnhacers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhacers(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
