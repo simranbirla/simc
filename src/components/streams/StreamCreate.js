be5df7632a7302895317297f6b9e73deb50e2542
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-08-01 01:43:43 +0530

    display error

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -3,10 +3,13 @@ import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
   renderInput(formProps) {
+    console.log(formProps.meta);
+
     return (
       <div className="field">
         <label>{formProps.label}</label>
         <input type="text" placeholder="enter" {...formProps.input} />
+        <div>{formProps.meta.error}</div>
       </div>
     );
   }
@@ -36,4 +39,20 @@ class StreamCreate extends React.Component {
   }
 }
 
-export default reduxForm({ form: "streamCreate" })(StreamCreate);
+const formValidate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You must enter a title"; //The key i.e errors.(title) interacts with FIELD NAME
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description"; //th key interacts with the FIELD
+  }
+
+  return errors;
+};
+
+export default reduxForm({ form: "streamCreate", validate: formValidate })(
+  StreamCreate
+);
