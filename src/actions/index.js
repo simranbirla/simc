c4f4c4c74e1e8c965cf8ddfe59582bb3b4feb967
simranbirla <62006675+simranbirla@users.noreply.github.com>
2020-08-01 21:10:53 +0530

    delete reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,12 @@
-import { SIGN_IN, SIGN_OUT } from "./type";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from "./type";
 import streams from "../apis/streams";
 
 export const signIn = (userId) => {
@@ -11,6 +19,40 @@ export const signOut = () => {
 
 export const createStream = (formValues) => {
   return async (dispatch) => {
-    streams.post("/streams", formValues);
+    const response = await streams.post("/streams", formValues);
+
+    dispatch({ type: CREATE_STREAM, payload: response.data });
+  };
+};
+
+export const fetchStreams = () => {
+  return async (dispatch) => {
+    const response = await streams.get("/streams");
+
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
+  };
+};
+
+export const fetchStream = (id) => {
+  return async (dispatch) => {
+    const response = await streams.get(`/streams/${id}`);
+
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+  };
+};
+
+export const editStream = (id) => {
+  return async (dispatch) => {
+    const response = await streams.put(`/streams/${id}`);
+
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+  };
+};
+
+export const deleteStream = (id) => {
+  return async (dispatch) => {
+    const response = await streams.delete(`/streams/${id}`);
+
+    dispatch({ type: DELETE_STREAM, payload: id });
   };
 };
---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,25 @@
+import _ from "lodash";
+import {
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+  CREATE_STREAM,
+} from "../actions/type";
+
+const streamReducer = (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
+    default:
+      return state;
+  }
+};
+
+export default streamReducer;
